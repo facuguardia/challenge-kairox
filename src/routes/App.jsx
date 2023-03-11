8470748985726069b87ed9fcd78659fc6d38b2c7
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2023-03-11 20:05:53 -0300

    context-app

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,16 +5,20 @@ import PageListCharacter from "../pages/PageListCharacter";
 import LandingPage from "../pages/LandingPage";
 // Components
 import Character from "../components/Character";
+// Context
+import AppContext from "../context/AppContext";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/character/:name/" element={<Character />} />
-        <Route path="/character-list" element={<PageListCharacter />} />
-      </Routes>
+      <AppContext.Provider value={{}}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/character/:name/" element={<Character />} />
+          <Route path="/character-list" element={<PageListCharacter />} />
+        </Routes>
+      </AppContext.Provider>
     </div>
   );
 }
